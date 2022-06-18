5629a726aa3a19c98e365681a327aa4eea4f84e9
Marilynn Stone <hello@marilynnstone.com>
2022-06-18 16:31:37 +0000

    move setupInput to input.js

--- play.js
+++ play.js
@@ -1,21 +1,10 @@
 // import connect from client
-const {net, connect} = require("./client");
+const connect = require("./client");
 
-// setup interface to handle user input from stdin
+const setupInput = require("./input");
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+console.log("Connecting ...");
 
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
+connect();
 
 setupInput();
